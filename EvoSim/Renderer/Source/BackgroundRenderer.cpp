994a0b389dc84e1c4290a30d87089057cc28cec1
Koby Lewis <kobyjlewis@gmail.com>
2021-05-27 18:51:15 -0600

    cut off the black strip at the edge of the map

--- EvoSim/Renderer/Source/BackgroundRenderer.cpp
+++ EvoSim/Renderer/Source/BackgroundRenderer.cpp
@@ -34,7 +34,7 @@ void BackgroundRenderer::copyColors(BackgroundCell** bg) {
 	const int bOffset = 4;
 	for (unsigned int x = 0; x < WorldState::WORLD_WIDTH; x++) {
 		for (unsigned int y = 0; y < WorldState::WORLD_WIDTH; y++) {
-			unsigned int vertIndex = (x + y * (WorldState::WORLD_WIDTH + 1)) * vertWidth;
+			unsigned int vertIndex = (x + y * (WorldState::WORLD_WIDTH)) * vertWidth;
 			vertices[vertIndex + rOffset] = ((float)bg[x][y].r) / UCHAR_MAX;
 			vertices[vertIndex + gOffset] = ((float)bg[x][y].g) / UCHAR_MAX;
 			vertices[vertIndex + bOffset] = ((float)bg[x][y].b) / UCHAR_MAX;
@@ -43,7 +43,7 @@ void BackgroundRenderer::copyColors(BackgroundCell** bg) {
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	unsigned int verticesSize = sizeof(float) * (WorldState::WORLD_WIDTH + 1) * (WorldState::WORLD_WIDTH + 1) * vertWidth;
+	unsigned int verticesSize = sizeof(float) * (WorldState::WORLD_WIDTH) * (WorldState::WORLD_WIDTH) * vertWidth;
 	glBufferData(GL_ARRAY_BUFFER, verticesSize, vertices, GL_STREAM_DRAW);
 }
 
@@ -51,25 +51,25 @@ void BackgroundRenderer::initRenderData() {
 	const int coordWidth = 2;
 	const int colorWidth = 3;
 	const int vertWidth = coordWidth + colorWidth;
-	vertices = new float[(WorldState::WORLD_WIDTH + 1) * (WorldState::WORLD_WIDTH + 1) * vertWidth];
-	for (unsigned int x = 0; x < WorldState::WORLD_WIDTH + 1; x++) {
-		for (unsigned int y = 0; y < WorldState::WORLD_WIDTH + 1; y++) {
-			unsigned int index = (x + y * (WorldState::WORLD_WIDTH + 1)) * vertWidth;
+	vertices = new float[(WorldState::WORLD_WIDTH) * (WorldState::WORLD_WIDTH) * vertWidth];
+	for (unsigned int x = 0; x < WorldState::WORLD_WIDTH; x++) {
+		for (unsigned int y = 0; y < WorldState::WORLD_WIDTH; y++) {
+			unsigned int index = (x + y * (WorldState::WORLD_WIDTH)) * vertWidth;
 			vertices[index] = x;
 			vertices[index + 1] = y;
 		}
 	}
 
 	const int quadWidth = 6;
-	indices = new unsigned int[WorldState::WORLD_WIDTH * WorldState::WORLD_WIDTH * quadWidth];
-	for (unsigned int x = 0; x < WorldState::WORLD_WIDTH; x++) {
-		for (unsigned int y = 0; y < WorldState::WORLD_WIDTH; y++) {
-			unsigned int topRight = (x + 1) + y * (WorldState::WORLD_WIDTH + 1);
-			unsigned int bottomRight = (x + 1) + (y + 1) * (WorldState::WORLD_WIDTH + 1);
-			unsigned int bottomLeft = x + (y + 1) * (WorldState::WORLD_WIDTH + 1);
-			unsigned int topLeft = x + y * (WorldState::WORLD_WIDTH + 1);
+	indices = new unsigned int[(WorldState::WORLD_WIDTH - 1) * (WorldState::WORLD_WIDTH - 1) * quadWidth];
+	for (unsigned int x = 0; x < WorldState::WORLD_WIDTH - 1; x++) {
+		for (unsigned int y = 0; y < WorldState::WORLD_WIDTH - 1; y++) {
+			unsigned int topRight = (x + 1) + y * (WorldState::WORLD_WIDTH);
+			unsigned int bottomRight = (x + 1) + (y + 1) * (WorldState::WORLD_WIDTH);
+			unsigned int bottomLeft = x + (y + 1) * (WorldState::WORLD_WIDTH);
+			unsigned int topLeft = x + y * (WorldState::WORLD_WIDTH);
 			
-			unsigned int index = (x + y * WorldState::WORLD_WIDTH) * quadWidth;
+			unsigned int index = (x + y * (WorldState::WORLD_WIDTH - 1)) * quadWidth;
 			indices[index] = topRight;
 			indices[index + 1] = bottomRight;
 			indices[index + 2] = topLeft;
@@ -86,11 +86,11 @@ void BackgroundRenderer::initRenderData() {
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	unsigned int verticesSize = sizeof(float) * (WorldState::WORLD_WIDTH + 1) * (WorldState::WORLD_WIDTH + 1) * vertWidth;
+	unsigned int verticesSize = sizeof(float) * (WorldState::WORLD_WIDTH) * (WorldState::WORLD_WIDTH) * vertWidth;
 	glBufferData(GL_ARRAY_BUFFER, verticesSize, vertices, GL_STREAM_DRAW);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	unsigned int indicesSize = sizeof(unsigned int) * WorldState::WORLD_WIDTH * WorldState::WORLD_WIDTH * quadWidth;
+	unsigned int indicesSize = sizeof(unsigned int) * (WorldState::WORLD_WIDTH - 1) * (WorldState::WORLD_WIDTH - 1) * quadWidth;
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesSize, indices, GL_STREAM_DRAW);
 
 	glVertexAttribPointer(0, coordWidth, GL_FLOAT, GL_FALSE, vertWidth * sizeof(float), (void*)0);
